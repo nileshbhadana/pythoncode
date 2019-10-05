e942d30c97c7219155687fee6b6917a22c8fb788
tushargoyal-hack1 <56203660+tushargoyal-hack1@users.noreply.github.com>
2019-10-06 00:16:56 +0530

    some update with message flow and commands

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import  socket 
-#            type of ip v4 ,      type of protocol UDP  
+import  socket #importing socket module for connection
+#            type of ip v4 ,      type of protocol UDP 
+
+# creaing the socket object
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 
 for i in range(10000): 
 	msg=raw_input("Enter command")
+	print("message you are sending", msg)
 	s.sendto(msg,("192.168.10.182",8000))
 
 
